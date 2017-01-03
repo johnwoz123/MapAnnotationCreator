48f03c08e57aa135e0303d3909e1bfbbe57c411a
john <jwozniak.homestar@gmail.com>
2017-01-03 10:00:56 -0500

    Initial Commit

---
+++ MapTableViewMockUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapTableViewMockUp
+//
+//  Created by john wozniak on 1/3/17.
+//  Copyright © 2017 john wozniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
